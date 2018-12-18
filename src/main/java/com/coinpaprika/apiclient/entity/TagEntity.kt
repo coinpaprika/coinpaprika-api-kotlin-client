e9f1401b78af7ba021a47dc4e826a558af6845a7
pkostecki <piotr.kst@gmail.com>
2018-12-18 12:58:28 +0100

    Retain state bug fixed

--- src/main/java/com/coinpaprika/apiclient/entity/TagEntity.kt
+++ src/main/java/com/coinpaprika/apiclient/entity/TagEntity.kt
@@ -14,8 +14,8 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class TagEntity(val id: String,
                      val name: String,
-                     val description: String,
-                     val type: String,
+                     val description: String? = "",
+                     val type: String? = "",
                      @SerializedName("coin_counter") val coinCounter: Int,
                      @SerializedName("ico_counter") val icoCounter: Int,
-                     val coins: List<String>): Parcelable
+                     val coins: List<String>? = emptyList()): Parcelable
