97ca46dedc775e3c2fe4c38a69f503aac5b4096d
pkostecki <piotr.kst@gmail.com>
2019-01-14 17:21:54 +0100

    Global Stats Entity updated

--- src/main/java/com/coinpaprika/apiclient/entity/GlobalStatsEntity.kt
+++ src/main/java/com/coinpaprika/apiclient/entity/GlobalStatsEntity.kt
@@ -3,7 +3,11 @@ package com.coinpaprika.apiclient.entity
 import com.google.gson.annotations.SerializedName
 
 data class GlobalStatsEntity(@SerializedName("market_cap_usd") val marketCapUsd: Long,
+                             @SerializedName("market_cap_change_24h") val dailyMarketCapChange: Double,
+                             @SerializedName("market_cap_ath_value") val marketCapAthValue: Long,
                              @SerializedName("volume_24h_usd") val dailyVolumeUsd: Long,
+                             @SerializedName("volume_24h_change_24h") val dailyVolumeChange: Double,
+                             @SerializedName("volume_24h_ath_value") val volumeAthValue: Long,
                              @SerializedName("bitcoin_dominance_percentage") val btcDominancePercentage: Double,
                              @SerializedName("cryptocurrencies_number") val cryptocurrenciesAmount: Int,
                              @SerializedName("last_updated") val lastUpdated: Long)
