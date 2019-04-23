212ece1f3d38fbb504c397f7262033cb4b1c3539
pkostecki <piotr.kst@gmail.com>
2019-04-23 13:06:31 +0200

    Gradle update

--- src/main/java/com/coinpaprika/apiclient/exception/NetworkConnectionException.kt
+++ src/main/java/com/coinpaprika/apiclient/exception/NetworkConnectionException.kt
@@ -1,8 +1,5 @@
 package com.coinpaprika.apiclient.exception
 
-/**
- * Exception throw by the application when a there is a network connection exception.
- */
 class NetworkConnectionException : Exception {
     constructor() : super()
     constructor(cause: Throwable?) : super(cause)
