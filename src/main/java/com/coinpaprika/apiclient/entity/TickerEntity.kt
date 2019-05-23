1f1ae089370fa2cc2b36ab6616551e9d80d90631
pkostecki <piotr.kst@gmail.com>
2019-05-23 16:07:50 +0200

    Model update

--- src/main/java/com/coinpaprika/apiclient/entity/TickerEntity.kt
+++ src/main/java/com/coinpaprika/apiclient/entity/TickerEntity.kt
@@ -13,6 +13,6 @@ data class TickerEntity(val id: String,
                         @SerializedName("total_supply") val totalSupply: Long = 0,
                         @SerializedName("max_supply") val maxSupply: Long = 0,
                         @SerializedName("beta_value") val betaValue: Double = 0.0,
-                        @SerializedName("last_updated") val lastUpdated: String = "",
+                        @SerializedName("last_updated") val lastUpdated: String? = "",
                         var quotes: Map<String, QuoteEntity>? = emptyMap(),
                         var tags: MutableList<String>? = mutableListOf()): Parcelable
