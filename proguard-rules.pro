coinpaprika/changelly-api-kotlin-client

Obfuscation disabled, proguard rules updated

proguard-rules.pro
@@ -19,3 +19,4 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+-keep public class com.coinpaprika.changellyapiclient.** { public *;}

coinpaprika/coinpaprika-api-kotlin-client

proguard-rules.pro
@@ -19,6 +19,7 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+-keep public class com.coinpaprika.apiclient.** { public *;}
 -keep class com.coinpaprika.apiclient.entity.** { *; }
 -dontwarn retrofit2.**
 -dontwarn okhttp3.**