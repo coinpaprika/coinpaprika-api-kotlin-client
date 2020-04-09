6ffde70d4cd147d49115d49a1df2446789301d20
Piotr Krystyniak <krpiotrek@gmail.com>
2020-04-09 10:13:18 +0200

    Parameterize http client (#6)

--- src/main/java/com/coinpaprika/apiclient/api/CoinpaprikaApiFactory.kt
+++ src/main/java/com/coinpaprika/apiclient/api/CoinpaprikaApiFactory.kt
@@ -12,7 +12,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-open class CoinpaprikaApiFactory {
+
+fun defaultClient(): OkHttpClient.Builder {
+    return OkHttpClient.Builder()
+        .protocols(listOf(Protocol.HTTP_1_1, Protocol.HTTP_2))
+        .writeTimeout(20000, TimeUnit.MILLISECONDS)
+        .readTimeout(10000, TimeUnit.MILLISECONDS)
+        .retryOnConnectionFailure(true)
+        .connectTimeout(15000, TimeUnit.MILLISECONDS)
+        .connectionPool(ConnectionPool(0, 1, TimeUnit.NANOSECONDS))
+}
+
+class CoinpaprikaApiFactory(private val client: OkHttpClient = defaultClient().build()) {
     companion object {
         private const val BASE_URL = "https://api.coinpaprika.com/v1/"
     }
@@ -22,17 +33,7 @@ open class CoinpaprikaApiFactory {
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .client(createClient().build())
+            .client(client)
             .build()
-    }
-
-    protected fun createClient(): OkHttpClient.Builder {
-        return OkHttpClient.Builder()
-            .protocols(listOf(Protocol.HTTP_1_1, Protocol.HTTP_2))
-            .writeTimeout(20000, TimeUnit.MILLISECONDS)
-            .readTimeout(10000, TimeUnit.MILLISECONDS)
-            .retryOnConnectionFailure(true)
-            .connectTimeout(15000, TimeUnit.MILLISECONDS)
-            .connectionPool(ConnectionPool(0, 1, TimeUnit.NANOSECONDS))
     }
 }
