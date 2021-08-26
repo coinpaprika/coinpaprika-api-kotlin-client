d089e9df61093daf554ba2a1c55246de9a616547
Bartek Wesołowski <bartas.wesolowski@gmail.com>
2021-08-26 16:05:25 +0200

    Remove unused TooManyRequestsError (#23)

--- src/main/java/com/coinpaprika/apiclient/exception/TooManyRequestsError.kt
+++
@@ -1,9 +0,0 @@
-package com.coinpaprika.apiclient.exception
-
-/**
- * Exception throw by the application when application is using API massively
- */
-class TooManyRequestsError : Exception {
-    constructor() : super()
-    constructor(cause: Throwable?) : super(cause)
-}
