412b67beee78df9788c26a12ce6605a0873c2312
Bartosz Wesolowski <bartosz.wesolowski@allegro.pl>
2021-09-02 10:20:21 +0200

    Refactor NewsApiContract

--- src/main/java/com/coinpaprika/apiclient/NewsApiContract.kt
+++ src/main/java/com/coinpaprika/apiclient/NewsApiContract.kt
@@ -1,12 +1,11 @@
 package com.coinpaprika.apiclient
 
 import com.coinpaprika.apiclient.entity.NewsEntity
-import io.reactivex.Observable
-import retrofit2.Response
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiContract {
     @GET("news/latest/")
-    fun getNews(@Query("limit") limit: Int): Observable<Response<List<NewsEntity>>>
+    fun getNews(@Query("limit") limit: Int): Single<List<NewsEntity>>
 }
