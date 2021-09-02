ce9bb0659323f6367bfb4732a82a0ca23966d4c1
Bartosz Wesolowski <bartosz.wesolowski@allegro.pl>
2021-09-02 10:07:02 +0200

    Refactor CoinApiContract

--- src/main/java/com/coinpaprika/apiclient/CoinApiContract.kt
+++ src/main/java/com/coinpaprika/apiclient/CoinApiContract.kt
@@ -1,29 +1,54 @@
 package com.coinpaprika.apiclient
 
-import com.coinpaprika.apiclient.entity.*
-import io.reactivex.Observable
-import retrofit2.Response
-import retrofit2.http.*
+import com.coinpaprika.apiclient.entity.CoinDetailsEntity
+import com.coinpaprika.apiclient.entity.CoinEntity
+import com.coinpaprika.apiclient.entity.EventEntity
+import com.coinpaprika.apiclient.entity.ExchangeEntity
+import com.coinpaprika.apiclient.entity.MarketEntity
+import com.coinpaprika.apiclient.entity.TweetEntity
+import io.reactivex.Completable
+import io.reactivex.Single
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CoinApiContract {
     @GET("coins/{id}")
-    suspend fun getCoin(@Path("id") id: String): CoinDetailsEntity
+    suspend fun getCoin(
+        @Path("id") id: String
+    ): CoinDetailsEntity
 
     @GET("coins")
-    suspend fun getCoins(@Query("additional_fields") additionalFields: String? = null): List<CoinEntity>
+    suspend fun getCoins(
+        @Query("additional_fields") additionalFields: String? = null
+    ): List<CoinEntity>
 
     @GET("coins/{id}/events/")
-    fun getEvents(@Path("id") id: String): Observable<Response<List<EventEntity>>>
+    fun getEvents(
+        @Path("id") id: String
+    ): Single<List<EventEntity>>
 
     @GET("coins/{id}/exchanges/")
-    fun getExchanges(@Path("id") id: String): Observable<Response<List<ExchangeEntity>>>
+    suspend fun getExchanges(
+        @Path("id") id: String
+    ): List<ExchangeEntity>
 
     @GET("coins/{id}/markets/")
-    fun getMarkets(@Path("id") id: String, @Query("quotes") quotes: String): Observable<Response<List<MarketEntity>>>
+    fun getMarkets(
+        @Path("id") id: String,
+        @Query("quotes") quotes: String
+    ): Single<List<MarketEntity>>
 
     @GET("coins/{id}/twitter/")
-    fun getTweets(@Path("id") id: String): Observable<Response<List<TweetEntity>>>
+    fun getTweets(
+        @Path("id") id: String
+    ): Single<List<TweetEntity>>
 
     @POST("coins/{id}/events")
-    fun addEvent(@Path("id") cryptoId: String, @Body event: EventEntity): Observable<Response<Void>>
+    fun addEvent(
+        @Path("id") cryptoId: String,
+        @Body event: EventEntity
+    ): Completable
 }
