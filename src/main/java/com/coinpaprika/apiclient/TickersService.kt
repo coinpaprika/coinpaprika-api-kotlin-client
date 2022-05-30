7c97e0319140460ce9b28df0820fb4defcd70a43
Mieszko Stelmach <mieszko.stelmach@gmail.com>
2022-05-30 21:40:00 +0200

    Add options to tickers API as optional QueryMap. Bumped Kotlin version to 1.6.21 (and related KSP). Bumped Gradle Wrapper version to 7.4.2

--- src/main/java/com/coinpaprika/apiclient/TickersService.kt
+++ src/main/java/com/coinpaprika/apiclient/TickersService.kt
@@ -4,17 +4,20 @@ import com.coinpaprika.apiclient.entity.TickerEntity
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.QueryMap
 
 interface TickersService {
     @GET("tickers/{id}/")
     suspend fun getTicker(
         @Path("id") id: String,
-        @Query("quotes") quotes: String
+        @Query("quotes") quotes: String,
+        @QueryMap options: Map<String, String> = emptyMap()
     ): TickerEntity
 
     @GET("tickers")
     suspend fun getTickers(
         @Query("quotes") quotes: String,
-        @Query("page") page: Int? = null
+        @Query("page") page: Int? = null,
+        @QueryMap options: Map<String, String> = emptyMap()
     ): List<TickerEntity>
 }
